59703aa922b1d248df77a309686eb531114c5ec2
miketheo423 <miketheo423@gmail.com>
2017-10-18 23:47:34 -0600

    Implements search bar functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,8 +13,8 @@ $(function () {
 		$("img").addClass('col-md-3').attr('id', 'api-movies');
 	});
 
-
-	$('#genre-btn').on('click', function() {
+	// Change populated movies based on genre dropdown
+	$('#genre-btn-movies').on('click', function() {
 		event.preventDefault();
 		let genreId = $('#genres option:selected').val();
 		// Make the get request for the movies depending on the genre
@@ -23,14 +23,82 @@ $(function () {
 			let movieList = data.results;
 			let moviePoster = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
 
+			// Iterate through the results array and display the results on the page.
+			for (let i = 0; i < movieList.length; i++) {
+				// Creates images and takes path from results to create the src
+				$('#movie-list').append(('<a href="http://google.com">'+'<img src="' + moviePoster + movieList[i].poster_path + '">' + '</a>'));
+			}
+			// Gives the imgs a bootstrap class to display four in a row.
+			$("img").addClass('col-md-3').attr('id', 'api-entertainment');
+		});
+	});
+
+	// Prevents enter key from refreshing page
+	$('#search-bar').submit(function() {
+		event.preventDefault();
+	});
+
+	// Calls api to search when search form is submitted
+	$('#search-bar-btn').on('click', function() {
+		event.preventDefault();
+		let searchQuery = $('#search-bar').val();
+		console.log(searchQuery);
+		// Make the get request for shows or movies depening on the search query
+		$.get('https://api.themoviedb.org/3/search/multi?api_key=868e357d0f927691ad60e3d98a0ecde4&language=en-US&query=' + searchQuery).done(function(data) {
+			console.log(data);
+			$('#movie-list').empty();
+			let mediaList = data.results;
+			let mediaPoster = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
+
+			// Iterate through the results array and display the results on the page.
+			for (let i = 0; i < mediaList.length; i++) {
+				// Creates images and takes path from results to create the src
+				if (mediaList[i].title !== undefined && mediaList[i].poster_path !==null) {
+				$('#movie-list').append(('<a href="http://google.com">'+'<img src="' + mediaPoster + mediaList[i].poster_path + '">' + '</a>'));
+				// Gives the imgs a bootstrap class to display four in a row.
+				$("img").addClass('col-md-3').attr('id', 'api-entertainment');
+				}
+			}
+		});
+		//clear the search form
+		$('#search-var').val('');
+	});
+
+
+	// Pull the discover movies api and make it into an object
+	let getTv = $.get('https://api.themoviedb.org/3/discover/tv?api_key=868e357d0f927691ad60e3d98a0ecde4&language=en-US&sort_by=popularity.desc&page=1').done(function(data) {
+		let tvList = data.results;
+		let tvPoster = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
+
 		// Iterate through the results array and display the results on the page.
-		for (let i = 0; i < movieList.length; i++) {
+		for (let i = 0; i < tvList.length; i++) {
 			// Creates images and takes path from results to create the src
-			$('#movie-list').append(('<a href="http://google.com">'+'<img src="' + moviePoster + movieList[i].poster_path + '">' + '</a>'));
+			$('#movie-list').append(('<a href="http://google.com">'+'<img src="' + tvPoster + tvList[i].poster_path + '">' + '</a>'));
 		}
 		// Gives the imgs a bootstrap class to display four in a row.
-		$("img").addClass('col-md-3').attr('id', 'api-movies');
+		$("img").addClass('col-md-3').attr('id', 'api-entertainment');
+	});
 
+	
+
+
+	// Gets tv shows based on genre
+	$('#genre-btn-tv').on('click', function() {
+		event.preventDefault();
+		let genreId = ('#genres option:selected').val();
+		//Make the get request for tv shows depending on the genre
+		$.get('https://api.themoviedb.org/3/discover/tv?api_key=868e357d0f927691ad60e3d98a0ecde4&language=en-US&sort_by=popularity.desc&page=1&with_genres=' + genreId).done(function(data) {
+		$('#movie-list').empty();
+			let tvList = data.results;
+			let tvPoster = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
+
+			// Iterate through the results array and display the results on the page.
+			for (let i = 0; i < tvList.length; i++) {
+				// Creates images and takes path from results to create the src
+				$('#movie-list').append(('<a href="http://google.com">'+'<img src="' + tvPoster + tvList[i].poster_path + '">' + '</a>'));
+			}
+			// Gives the imgs a bootstrap class to display four in a row.
+			$("img").addClass('col-md-3').attr('id', 'api-entertainment');
 		});
 	});
 
